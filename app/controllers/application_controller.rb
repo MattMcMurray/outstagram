d25f3e9168142aba4f64acbfa38e54daca1867e1
Matt McMurray <MattMcMurray@users.noreply.github.com>
2020-09-12 10:38:44 -0500

    Add user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    # TODO: match to user's current path if not coming from welcome screen
+    "/feed"
+  end
 end
