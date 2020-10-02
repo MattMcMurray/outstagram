6bd491009ad504b4730ec7b10a7c327a4fb51e45
Mathieu McMurray <mathieumcmurray@gmail.com>
2020-10-02 10:30:09 -0500

    Bugfix: If not following any users, return empty list

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,7 +2,11 @@ class FeedController < ApplicationController
 
   def index
     @users = FollowRelationship.find_by(follower_id: current_user.id)
-    @posts = Post.joins(:user).where(posts: {user_id: @users.id})
+    if @users != nil
+      @posts = Post.joins(:user).where(posts: {user_id: @users.id})
+    else
+      @posts = []
+    end
   end
 
   def explore
