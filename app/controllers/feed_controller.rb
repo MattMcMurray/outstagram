84b938cffa5d47c0ea1d282f70cdab226b4dcb35
Mathieu McMurray <mathieumcmurray@gmail.com>
2020-10-24 12:56:05 -0500

    BUGFIX: controller was fetching posts on wrong id
    - Was using relationship id instead of user id

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,7 +3,9 @@ class FeedController < ApplicationController
   def index
     @users = FollowRelationship.find_by(follower_id: current_user.id)
     if @users != nil
-      @posts = Post.joins(:user).where(posts: {user_id: @users.id})
+      @posts = Post.joins(:user)
+                .where(posts: {user_id: @users.followee_id})
+                .or(Post.joins(:user).where(posts: {user_id: current_user.id}))
     else
       @posts = []
     end
